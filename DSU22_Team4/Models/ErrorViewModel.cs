9c7ea9f2d9ef03f8ad9754c6fa8a6d4c29e8908c
NeboT1000 <73859153+NeboT1000@users.noreply.github.com>
2022-01-26 10:07:54 +0100

    Add project files.

---
+++ DSU22_Team4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSU22_Team4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
