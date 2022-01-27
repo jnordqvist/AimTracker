0a0da87ea3335de5e51082e3ebeee5e606a6a34a
annerklev <alexandraannerklev@gmail.com>
2022-01-27 15:58:28 +0100

    Started to create header

--- DSU22_Team4/wwwroot/js/site.js
+++ DSU22_Team4/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
