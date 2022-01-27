1be582ce4e284e9c2d5b5d5400f10b4f62f7a6d8
Joakim Nordqvist <jono1511@student.miun.se>
2022-01-27 14:27:36 +0100

    Created methods for session total percentage and session average percentage

--- DSU22_Team4/Models/ViewModels/HomeViewModel.cs
+++ DSU22_Team4/Models/ViewModels/HomeViewModel.cs
@@ -15,7 +15,82 @@ namespace DSU22_Team4.Models.ViewModels
         {
             Session = session;
             Series = session.Series;
+            // CalculateSeriesHitPercentage(Series.FirstOrDefault());
+            GetSessionTotalHitPercentage(session);
+            GetSessionAverageHitPercentage(session);
             //Date = session.Date;
+        }
+
+        public string GetSeriesHitPercentage(Serie serie)
+        {
+            string result = $"{CalculateHitPercentage(serie) * 100}%";
+            return result;
+        }
+
+        public double CalculateHitPercentage(Serie serie)
+        {
+            double counter = 0;
+            foreach (var shot in serie.Shots)
+            {
+                if (shot.Result == "hit")
+                {
+                    counter += 1;
+                }
+
+            }
+            double temp = (double)serie.Shots.Count;
+            double hitPercentage = counter / temp;
+            return hitPercentage;
+        }
+
+        public string GetSessionAverageHitPercentage(TrainingSession session)
+        {
+            double hitPercentage = 0;
+            foreach (var serie in session.Series)
+            {
+                hitPercentage += CalculateHitPercentage(serie);
+            }
+            double series = (double)session.Series.Count();
+            double result = hitPercentage / series;
+            return $"{result * 100}%";
+        }
+
+        public int GetTotalNumOfShots(TrainingSession session)
+        {
+            int shots = 0;
+            foreach (var serie in session.Series)
+            {
+                foreach (var shot in serie.Shots)
+                {
+                    shots += 1;
+                }
+            }
+            return shots;
+        }
+
+        public int GetTotalNumOfHits(TrainingSession session)
+        {
+            int hits = 0;
+            foreach (var serie in session.Series)
+            {
+                foreach (var shot in serie.Shots)
+                {
+                    if (shot.Result == "hit")
+                    {
+                        hits += 1;
+                    }
+                }
+            }
+            return hits;
+        }
+
+        public string GetSessionTotalHitPercentage(TrainingSession session)
+        {
+            int shots = GetTotalNumOfShots(session);
+            int hits = GetTotalNumOfHits(session);
+
+            double hitPercentage = Math.Round((((double)hits / (double)shots) * 100), 2);
+            return $"{hitPercentage}%";
         }
     }
 }
