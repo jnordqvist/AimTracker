349ca63df01cdfae0be762ad69d3cf58ae2f959e
annerklev <alexandraannerklev@gmail.com>
2022-01-28 08:59:12 +0100

    Ugly but responsive header fixed. To be continued.

--- DSU22_Team4/wwwroot/js/site.js
+++ DSU22_Team4/wwwroot/js/site.js
@@ -2,15 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Declaration of necessary variables 
+var topnav = document.getElementById("myLinks");
+var mediaquery = window.matchMedia("(min-width: 1000px)")
+
+displayLinks(mediaquery) // Calls function on runtime
+
+mediaquery.addListener(displayLinks) // Attach listener function on state changes
+
+// Show and hide links depending on topnav style 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+ 
+    if (topnav.style.display === "block") {
+        topnav.style.display = "none";
 
     } else {
-        x.style.display = "block";
+        topnav.style.display = "block";
     }
 
 
-
 }
+
+// Displays links when media query is equal to 1000px or > 
+function displayLinks(mediaquery) {
+    if (mediaquery.matches) { 
+        topnav.style.display = "flex";
+    } else {
+        topnav.style.display = "none";
+    }
+}
+
+
