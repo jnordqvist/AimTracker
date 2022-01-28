1b0f42b3d4c3a5faa056946c9eb980527008fc0e
Joakim Nordqvist <jono1511@student.miun.se>
2022-01-28 11:46:57 +0100

    testdatabase filled with data from mock json

--- DSU22_Team4/Data/AppDbContext.cs
+++ DSU22_Team4/Data/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace DSU22_Team4.Data
 {
     public class AppDbContext: DbContext
     {    
-        public DbSet <TrainingSession> TrainingSessions { get; set; }
+        public DbSet <Athlete> Athlete { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
                 
