49c74d45aa78c6f6a4abf5944b870dcb1525780f
annerklev <alexandraannerklev@gmail.com>
2022-02-14 18:03:55 +0100

    first trainingsession out and alot of csshtml after that

---
+++ DSU22_Team4/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DSU22_Team4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new int[] { 860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478 };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+      
+
+      
+    }
+}
