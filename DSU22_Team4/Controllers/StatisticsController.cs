d176f154ed3429760859dafdcf21b2f27f31aaed
junofallbom <73644576+junofallbom@users.noreply.github.com>
2022-02-17 22:40:27 +0100

    fixed statistics page for presentation

--- DSU22_Team4/Controllers/StatisticsController.cs
+++ DSU22_Team4/Controllers/StatisticsController.cs
@@ -45,10 +45,53 @@ namespace DSU22_Team4.Controllers
             var sessions = GetTrainingSessions(athlete);
             var defaultSession = getDefaultSession(id);
             int intensity = _statsdb.TrainingSessionIntensity();
-            var statisticsViewModel = new StatisticsViewModel(athlete, defaultSession, intensity);
-           // statisticsViewModel.DropDownTrainingSessions = _statsdb.GetTrainingSessions(athlete.Id);
+            var statisticsViewModel = new StatisticsViewModel(athlete, defaultSession, sessions);
+            //statisticsViewModel.DropDownTrainingSessions = _statsdb.GetTrainingSessions(athlete.Id);
 
             return View(statisticsViewModel);
         }
     }
+    //public class StatisticsController : Controller
+    //{
+    //    private readonly IDbRepository _statsdb;
+    //    private IEnumerable<SelectListItem> GetTrainingSessions(Athlete athlete)
+    //    {
+    //        var ddtrainingsessions = _statsdb
+    //                    .GetTrainingSessions(athlete.Id)
+    //                    .Select(x =>
+    //                            new SelectListItem
+    //                            {
+    //                                Value = x.Date.ToString(),
+    //                                Text = x.Date.ToString()
+    //                            });
+
+    //        return new SelectList(ddtrainingsessions, "Value", "Text");
+    //    }
+
+    //    private TrainingSession getDefaultSession(string id)
+    //    {
+    //        var sessions = _statsdb.GetTrainingSessions(id);
+    //        return sessions.FirstOrDefault();
+    //    }
+
+    //    public StatisticsController(IDbRepository statsdb)
+    //    {
+    //        _statsdb = statsdb;
+    //    }
+
+    //    public async Task<IActionResult> Index(string id)
+    //    {
+    //        await Task.Delay(0);
+    //        var athlete = _statsdb.GetAthleteById(id);
+    //        var sessions = GetTrainingSessions(athlete);
+    //        var defaultSession = getDefaultSession(id);
+    //        int intensity = _statsdb.TrainingSessionIntensity();
+    //        var statisticsViewModel = new StatisticsViewModel(athlete, defaultSession, sessions, intensity);
+    //        // statisticsViewModel.DropDownTrainingSessions = _statsdb.GetTrainingSessions(athlete.Id);
+
+    //        return View(statisticsViewModel);
+    //    }
+    //}
+//}
+
 }
